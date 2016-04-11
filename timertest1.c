6114916f5c3360efbed8a83c9c6768d3fc9825a2
Andy Gallagher <andy.gallagher@theguardian.com>
2016-04-11 10:10:41 +0100

    set up timer test to re-call itself, no problems with that nor unloading.  Including eclipse project definition.

--- timertest1.c
+++ timertest1.c
@@ -8,7 +8,10 @@ static struct timer_list my_timer;
 
 void my_timer_callback(unsigned long data)
 {
+	int ret;
 	printk("my_timer_callback called (%ld).\n", jiffies);
+	ret = mod_timer(&my_timer, jiffies + msecs_to_jiffies(2000));
+	if(ret) printk("Error in mod_timer\n");
 }
 
 int init_module(void)
@@ -19,7 +22,7 @@ int init_module(void)
 	setup_timer(&my_timer, my_timer_callback, 0);
 
 	printk("Starting timer to fire in 1000ms (%ld)\n", jiffies);
-	ret = mod_timer(&my_timer, jiffies + msecs_to_jiffies(1000));
+	ret = mod_timer(&my_timer, jiffies + msecs_to_jiffies(2000));
 	if(ret) printk("Error in mod_timer\n");
 
 	return 0;
